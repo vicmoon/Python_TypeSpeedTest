dee4d82dabcd09f2a0cb317b36d3c8c0af184786
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2025-02-21 14:54:58 +0100

    add  Procfile and requirements

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for, request, session
 from words import words
 import time
+import os
 
 #create the app 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "BIgSecret550600"
+# app.config["SECRET_KEY"] = "BIgSecret550600"
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
 
 
 def get_words():
